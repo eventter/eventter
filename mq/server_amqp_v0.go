d533b0a8d82c42016b41ac1b69478c91303107fe
Jakub Kulhan <jakub.kulhan@gmail.com>
2018-12-26 00:58:29 +0100

    mq: amqp 0.9.1 - queue.unbind

--- mq/server_amqp_v0.go
+++ mq/server_amqp_v0.go
@@ -476,6 +476,86 @@ func (s *Server) handleAMQPv0ChannelMethod(ctx context.Context, transport *v0.Tr
 			FrameMeta: v0.FrameMeta{Channel: ch.id},
 		})
 
+	case *v0.QueueUnbind:
+		state := s.clusterState.Current()
+		namespace, _ := state.FindNamespace(namespaceName)
+		if namespace == nil {
+			return s.makeChannelClose(ch, v0.NotFound, errors.Errorf("vhost %q not found", namespaceName))
+		}
+
+		cg, _ := namespace.FindConsumerGroup(frame.Queue)
+		if cg == nil {
+			return s.makeChannelClose(ch, v0.NotFound, errors.Errorf("queue %q not found", frame.Queue))
+		}
+
+		tp, _ := namespace.FindTopic(frame.Exchange)
+		if tp == nil {
+			return s.makeChannelClose(ch, v0.NotFound, errors.Errorf("exchange %q not found", frame.Exchange))
+		}
+
+		unbinding := &client.ConsumerGroup_Binding{
+			TopicName: tp.Name,
+		}
+
+		switch tp.Type {
+		case client.ExchangeTypeFanout:
+			// do nothing
+		case client.ExchangeTypeDirect:
+			fallthrough
+		case client.ExchangeTypeTopic:
+			unbinding.By = &client.ConsumerGroup_Binding_RoutingKey{RoutingKey: frame.RoutingKey}
+		case client.ExchangeTypeHeaders:
+			if frame.Arguments == nil || frame.Arguments.Fields == nil {
+				return s.makeConnectionClose(v0.SyntaxError, errors.New("trying to bind to headers exchange, but arguments not set"))
+			}
+
+			algo, err := structvalue.String(frame.Arguments, "x-match", "")
+			if err != nil {
+				return s.makeConnectionClose(v0.SyntaxError, errors.Wrap(err, "x-match field failed"))
+			}
+
+			delete(frame.Arguments.Fields, "x-match")
+
+			switch algo {
+			case "":
+				return s.makeConnectionClose(v0.SyntaxError, errors.Errorf("trying to bind to headers exchange, but %q not set", "x-match"))
+			case "all":
+				unbinding.By = &client.ConsumerGroup_Binding_HeadersAll{HeadersAll: frame.Arguments}
+			case "any":
+				unbinding.By = &client.ConsumerGroup_Binding_HeadersAny{HeadersAny: frame.Arguments}
+			default:
+				return s.makeConnectionClose(v0.SyntaxError, errors.Errorf("unknown matching algorithm %q", algo))
+			}
+		default:
+			panic("unhandled exchange type")
+		}
+
+		request := &client.CreateConsumerGroupRequest{
+			ConsumerGroup: client.ConsumerGroup{
+				Name: client.NamespaceName{
+					Namespace: namespaceName,
+					Name:      frame.Queue,
+				},
+				Size_: cg.Size_,
+			},
+		}
+
+		for _, binding := range cg.Bindings {
+			clientBinding := s.convertBinding(binding)
+			if !reflect.DeepEqual(clientBinding, unbinding) {
+				request.ConsumerGroup.Bindings = append(request.ConsumerGroup.Bindings, clientBinding)
+			}
+		}
+
+		_, err := s.CreateConsumerGroup(ctx, request)
+		if err != nil {
+			return s.makeConnectionClose(v0.InternalError, errors.Wrap(err, "create failed"))
+		}
+
+		return transport.Send(&v0.QueueUnbindOk{
+			FrameMeta: v0.FrameMeta{Channel: ch.id},
+		})
+
 	default:
 		return s.makeConnectionClose(v0.SyntaxError, errors.Errorf("unexpected frame of type %T", frame))
 	}
