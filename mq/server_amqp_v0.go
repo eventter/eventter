a1150cad7e528a96895a12d51361e25860c3cde1
Jakub Kulhan <jakub.kulhan@gmail.com>
2018-12-25 01:57:39 +0100

    mq: amqp 0.9.1 - channel state & clean up

--- mq/server_amqp_v0.go
+++ mq/server_amqp_v0.go
@@ -9,6 +9,11 @@ import (
 	"github.com/pkg/errors"
 )
 
+const (
+	readyState   = 1
+	closingState = 2
+)
+
 func (s *Server) HandleAMQPv0(conn net.Conn, sc *v0.ServerConn) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -37,11 +42,15 @@ func (s *Server) HandleAMQPv0(conn net.Conn, sc *v0.ServerConn) error {
 	}()
 
 	channels := make(map[uint16]*serverAMQPv0Channel)
-
+	defer func() {
+		for _, ch := range channels {
+			ch.Close()
+		}
+	}()
 	for {
 		select {
 		case <-s.closeC:
-			return forceCloseAMQPv0Connection(conn, sc, v0.ConnectionForced, "shutdown")
+			return s.forceCloseAMQPv0(conn, sc, v0.ConnectionForced, "shutdown")
 		case frame := <-frames:
 			switch frame := frame.(type) {
 			case v0.MethodFrame:
@@ -49,101 +58,129 @@ func (s *Server) HandleAMQPv0(conn net.Conn, sc *v0.ServerConn) error {
 				if meta.Channel == 0 {
 					switch frame := frame.(type) {
 					case *v0.ConnectionClose:
-						if frame.ReplyCode == 0 || frame.ReplyCode == v0.ReplySuccess {
-							return sendAMQPv0(conn, sc, &v0.ConnectionCloseOk{})
-						} else {
+						if frame.ReplyCode != 0 && frame.ReplyCode != v0.ReplySuccess {
 							return errors.Errorf("received connection error %q (%d)", frame.ReplyText, frame.ReplyCode)
 						}
-					case *v0.ConnectionCloseOk:
-						return nil
+						return s.sendAMQPv0(conn, sc, &v0.ConnectionCloseOk{})
 					default:
-						return forceCloseAMQPv0Connection(conn, sc, v0.SyntaxError, "non-close frame")
+						return s.forceCloseAMQPv0(conn, sc, v0.SyntaxError, "non-close frame")
 					}
 				} else {
 					switch frame.(type) {
 					case *v0.ChannelOpen:
 						if _, ok := channels[meta.Channel]; ok {
-							return forceCloseAMQPv0Connection(conn, sc, v0.ChannelError, "channel already open")
+							return s.forceCloseAMQPv0(conn, sc, v0.ChannelError, "channel already open")
 						}
 						channels[meta.Channel] = &serverAMQPv0Channel{
-							// TODO
+							state: readyState,
 						}
-						err := sendAMQPv0(conn, sc, &v0.ChannelOpenOk{FrameMeta: v0.FrameMeta{Channel: meta.Channel}})
+						err := s.sendAMQPv0(conn, sc, &v0.ChannelOpenOk{FrameMeta: v0.FrameMeta{Channel: meta.Channel}})
 						if err != nil {
-							return forceCloseAMQPv0Connection(conn, sc, v0.InternalError, err.Error())
+							return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
+						}
+					case *v0.ChannelClose:
+						ch, ok := channels[meta.Channel]
+						if ok {
+							return s.forceCloseAMQPv0(conn, sc, v0.ChannelError, "channel not open")
+						}
+						delete(channels, meta.Channel)
+						err := ch.Close()
+						if err != nil {
+							return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
+						}
+						err = s.sendAMQPv0(conn, sc, &v0.ChannelCloseOk{FrameMeta: v0.FrameMeta{Channel: meta.Channel}})
+						if err != nil {
+							return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
+						}
+					case *v0.ChannelCloseOk:
+						ch, ok := channels[meta.Channel]
+						if !ok {
+							return s.forceCloseAMQPv0(conn, sc, v0.ChannelError, "channel not open")
+						}
+						if ch.state != closingState {
+							return s.forceCloseAMQPv0(conn, sc, v0.SyntaxError, "channel not closing")
+						}
+						delete(channels, meta.Channel)
+						err := ch.Close()
+						if err != nil {
+							return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
 						}
 					default:
 						ch, ok := channels[meta.Channel]
 						if !ok {
-							return forceCloseAMQPv0Connection(conn, sc, v0.ChannelError, "channel not open")
+							return s.forceCloseAMQPv0(conn, sc, v0.ChannelError, "channel not open")
 						}
 						err := s.handleAMQPv0ChannelMethod(conn, sc, ch, frame)
 						if err != nil {
 							if connClose, ok := err.(*v0.ConnectionClose); ok {
-								return sendAMQPv0(conn, sc, connClose)
+								return s.sendAMQPv0(conn, sc, connClose)
 							} else if chanClose, ok := err.(*v0.ChannelClose); ok {
-								err = sendAMQPv0(conn, sc, chanClose)
+								ch.state = closingState
+								err = s.sendAMQPv0(conn, sc, chanClose)
 								if err != nil {
-									return forceCloseAMQPv0Connection(conn, sc, v0.InternalError, err.Error())
+									return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
 								}
 							} else {
-								return forceCloseAMQPv0Connection(conn, sc, v0.InternalError, err.Error())
+								return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
 							}
 						}
 					}
 				}
 			case *v0.ContentHeaderFrame:
 				if frame.FrameMeta.Channel == 0 {
-					return forceCloseAMQPv0Connection(conn, sc, v0.SyntaxError, "content header frame on zero channel")
+					return s.forceCloseAMQPv0(conn, sc, v0.SyntaxError, "content header frame on zero channel")
 				}
 				ch, ok := channels[frame.FrameMeta.Channel]
 				if !ok {
-					return forceCloseAMQPv0Connection(conn, sc, v0.ChannelError, "channel not open")
+					return s.forceCloseAMQPv0(conn, sc, v0.ChannelError, "channel not open")
 				}
 				err := s.handleAMQPv0ChannelContentHeader(conn, sc, ch, frame)
 				if err != nil {
 					if connClose, ok := err.(*v0.ConnectionClose); ok {
-						return sendAMQPv0(conn, sc, connClose)
+						return s.sendAMQPv0(conn, sc, connClose)
 					} else if chanClose, ok := err.(*v0.ChannelClose); ok {
-						err = sendAMQPv0(conn, sc, chanClose)
+						ch.state = closingState
+						err = s.sendAMQPv0(conn, sc, chanClose)
 						if err != nil {
-							return forceCloseAMQPv0Connection(conn, sc, v0.InternalError, err.Error())
+							return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
 						}
 					} else {
-						return forceCloseAMQPv0Connection(conn, sc, v0.InternalError, err.Error())
+						return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
 					}
 				}
 			case *v0.ContentBodyFrame:
 				if frame.FrameMeta.Channel == 0 {
-					return forceCloseAMQPv0Connection(conn, sc, v0.SyntaxError, "content body frame on zero channel")
+					return s.forceCloseAMQPv0(conn, sc, v0.SyntaxError, "content body frame on zero channel")
 				}
 				ch, ok := channels[frame.FrameMeta.Channel]
 				if !ok {
-					return forceCloseAMQPv0Connection(conn, sc, v0.ChannelError, "channel not open")
+					return s.forceCloseAMQPv0(conn, sc, v0.ChannelError, "channel not open")
 				}
 				err := s.handleAMQPv0ChannelContentBody(conn, sc, ch, frame)
 				if err != nil {
 					if connClose, ok := err.(*v0.ConnectionClose); ok {
-						return sendAMQPv0(conn, sc, connClose)
+						return s.sendAMQPv0(conn, sc, connClose)
 					} else if chanClose, ok := err.(*v0.ChannelClose); ok {
-						err = sendAMQPv0(conn, sc, chanClose)
+						ch.state = closingState
+						err = s.sendAMQPv0(conn, sc, chanClose)
 						if err != nil {
-							return forceCloseAMQPv0Connection(conn, sc, v0.InternalError, err.Error())
+							return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
 						}
 					} else {
-						return forceCloseAMQPv0Connection(conn, sc, v0.InternalError, err.Error())
+						return s.forceCloseAMQPv0(conn, sc, v0.InternalError, err.Error())
 					}
 				}
 			case *v0.HeartbeatFrame:
 				if frame.FrameMeta.Channel != 0 {
-					return forceCloseAMQPv0Connection(conn, sc, v0.SyntaxError, "heartbeat frame on non-zero channel")
+					return s.forceCloseAMQPv0(conn, sc, v0.SyntaxError, "heartbeat frame on non-zero channel")
 				}
 			default:
 				return errors.Errorf("unhandled frame of type %T", frame)
 			}
 		case err := <-errc:
-			if err == v0.ErrMalformedFrame {
-				err = forceCloseAMQPv0Connection(conn, sc, v0.FrameError, "malformed frame")
+			cause := errors.Cause(err)
+			if cause == v0.ErrMalformedFrame {
+				err = s.forceCloseAMQPv0(conn, sc, v0.FrameError, "malformed frame")
 			}
 			return err
 		}
@@ -151,6 +188,11 @@ func (s *Server) HandleAMQPv0(conn net.Conn, sc *v0.ServerConn) error {
 }
 
 type serverAMQPv0Channel struct {
+	state int
+}
+
+func (ch *serverAMQPv0Channel) Close() error {
+	return nil
 }
 
 func (s *Server) handleAMQPv0ChannelMethod(conn net.Conn, sc *v0.ServerConn, ch *serverAMQPv0Channel, frame v0.MethodFrame) error {
@@ -165,7 +207,7 @@ func (s *Server) handleAMQPv0ChannelContentBody(conn net.Conn, sc *v0.ServerConn
 	panic("implement me")
 }
 
-func sendAMQPv0(conn net.Conn, sc *v0.ServerConn, frame v0.Frame) error {
+func (s *Server) sendAMQPv0(conn net.Conn, sc *v0.ServerConn, frame v0.Frame) error {
 	err := conn.SetWriteDeadline(time.Now().Add(sc.Heartbeat))
 	if err != nil {
 		return errors.Wrap(err, "set write deadline failed")
@@ -177,8 +219,8 @@ func sendAMQPv0(conn net.Conn, sc *v0.ServerConn, frame v0.Frame) error {
 	return nil
 }
 
-func forceCloseAMQPv0Connection(conn net.Conn, sc *v0.ServerConn, replyCode uint16, replyText string) error {
-	err := sendAMQPv0(conn, sc, &v0.ConnectionClose{
+func (s *Server) forceCloseAMQPv0(conn net.Conn, sc *v0.ServerConn, replyCode uint16, replyText string) error {
+	err := s.sendAMQPv0(conn, sc, &v0.ConnectionClose{
 		ReplyCode: replyCode,
 		ReplyText: replyText,
 	})
